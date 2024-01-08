05847693e92e3211debaf8cbc59ad3941bed5940
Dexter 259 <152500660+Dificil2016@users.noreply.github.com>
2024-01-08 13:02:21 +0100

    Vidas 0.5
    Barra de vida a medio hacer

---
+++ Spirits Journey/Assets/Uniproto/Components/UI/UP_StateBarUI.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+
+public class UP_StateBarUI : MonoBehaviour
+{
+    void Update()
+    {
+        
+    }
+}
