60d8d9f2b724352e4cf9591b637b237f1d4d5421
Dexter 259 <152500660+Dificil2016@users.noreply.github.com>
2024-01-12 16:32:33 +0100

    Update UP_StateBarUI.cs

--- Spirits Journey/Assets/Uniproto/Components/UI/UP_StateBarUI.cs
+++ Spirits Journey/Assets/Uniproto/Components/UI/UP_StateBarUI.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UP_StateBarUI : MonoBehaviour
 {
+
+    [SerializeField] Sprite[] sprites;
+
+    [SerializeField] UP_Counter counter = null;
+
+    [SerializeField] Image barImage = null;
+
+
     void Update()
     {
-        
+        if (counter.Counter <= sprites.Length)
+        {
+            int value = counter.Counter;
+            barImage.sprite = sprites[value];
+        }
     }
 }
